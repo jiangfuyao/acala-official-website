1fa0d66ede018cf98c2cf3f4ebc428713dc993d1
qwer951123 <qiuhao951@gmail.com>
2020-10-13 15:12:38 +0800

    switch jobs service provider to lever

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,7 +1,24 @@
-import React, { useEffect, createRef, useCallback } from 'react';
+import React, { useEffect, createRef, useCallback, useMemo, useState } from 'react';
+import axios from 'axios';
 import classes from './Jobs.module.css';
 import { Button } from '../ui-components/Button';
 import { Container } from '../ui-components/Container';
+
+const leverWebsite = 'https://jobs.lever.co/acala';
+
+const leverApi = 'https://api.lever.co/v0/postings/acala?mode=json';
+
+const useLever = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get(leverApi).then((result) => {
+            if (result.status === 200) setData(result.data); 
+        });
+    }, []);
+    
+    return data;
+};
 
 export const Jobs = () => {
     const ref = createRef();
@@ -10,26 +27,29 @@ export const Jobs = () => {
         window.open('https://medium.com/acalanetwork');
     }, []);
 
-    useEffect(() => {
-        if (!ref.current) return;
+    const jobs = useLever();
 
-        const $script = document.createElement('script');
-
-        $script.setAttribute('src', 'https://angel.co/javascripts/embed_jobs.js');
-        $script.setAttribute('id', 'angellist_embed');
-        $script.setAttribute('data-startup', 'acala-network');
-
-        ref.current.appendChild($script);
-    }, [ref]);
     return (
         <section className={classes.root}>
             <div className={classes.header}>
                 <p className={classes.title}>Want to Build the Future of Finance?</p>
                 <p className={classes.title}>Build with Acala</p>
-                <Button className={classes.viewCurrentOpening} link='https://angel.co/company/acala-network/jobs'>View Our Current Opening</Button>
+                <Button className={classes.viewCurrentOpening} link={leverWebsite}>View Our Current Opening</Button>
             </div>
             <Container className={classes.jobs} ref={ref}>
                 <p className={classes.subTitle}>Who we are looking for</p>
+                <div className={classes.jobsList}>
+                    {
+                        jobs ? jobs.map((item) => {
+                            return (
+                                <div key={item.text} className={classes.jobItem} onClick={() => window.open(item.hostedUrl)}>
+                                    <p className={classes.jobTitle}>{item.text}</p>
+                                    <p className={classes.jobCategories}>{`${item.categories.location} ${item.categories.team}`}</p>
+                                </div>
+                            );
+                        }) : <p>loading</p>
+                    }
+                </div>
             </Container>
 
             <Container className={classes.concat}>
